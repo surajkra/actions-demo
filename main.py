5efb7ec1114a2b203ecc70f73036b0f85207571d
Suraj Kiran Raman <rsurajkiran@gmail.com>
2022-04-04 23:45:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ import pprint
 if __name__=="__main__":
   print("Hi there from Python")
   #print(os.environ["UBUNTU_SERVER"])
-  pprint(dict(os.environ))
+  pprint.pprint(dict(os.environ), width = 1)
