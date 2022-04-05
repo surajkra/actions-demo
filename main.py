6c5a9acd4bb9b58e594d37910c6d92993680f172
Suraj Kiran Raman <rsurajkiran@gmail.com>
2022-04-04 23:44:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 import sys
+import pprint
 
 if __name__=="__main__":
   print("Hi there from Python")
   #print(os.environ["UBUNTU_SERVER"])
-  print(os.environ["credentials"])
+  pprint(dict(os.environ))
