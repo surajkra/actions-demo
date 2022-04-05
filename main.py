80ab48be637e6bf8e59b4703dfd2b050ba3ee2d8
Suraj Kiran Raman <rsurajkiran@gmail.com>
2022-04-05 00:28:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
+import requests
 import os
-import sys
-import pprint
 
-if __name__=="__main__":
-  print("Hi there from Python")
-  UBUNTU_SERVER = os.environ['UBUNTU_SERVER']
-  POLYGON_API = os.environ['POLYGON_API_KEY']
-  #print(os.environ["UBUNTU_SERVER"])
-  print(UBUNTU_SERVER)
-  pprint.pprint(dict(os.environ))
+def send_request(stock_ticker: str, date: str, api_key: str, base_url: str = 'https://api.polygon.io/v1/open-close/',
+                 adjusted_flag: str = 'true'):
+    """
+    Function to send API request to polygon API
+
+    :param stock_ticker: str abbreviation of company name
+    :param date: str date of stock info
+    :param api_key: str API-key for polygon API
+    :param base_url: str base url format
+    :param adjusted_flag: str whether to get adjusted value or not. Default: true
+
+    :return: dict containing result
+
+    """
+    url = f'{base_url}{stock_ticker}/{date}'
+    params = {'adjusted': adjusted_flag,
+              'apiKey': api_key}
+
+    api_request = requests.get(url=url, params=params)
+    if api_request.status_code == 200:
+        output = api_request.json()
+    else:
+        raise AssertionError(f"API request not successful: status code {api_request.status_code}")
+
+    return output
+
+
+if __name__ == "__main__":
+    POLYGON_API_KEY = os.environ["POLYGON_API_KEY"]
+    #api_key = p7ch9DdAkyYJbuSzxAm94NRFj6TUrWfa
+    api_call = send_request(stock_ticker='AAPL', date='2022-01-05', api_key=POLYGON_API_KEY)
+    print(api_call)
+    
